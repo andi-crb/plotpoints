05a8aa44cf9bc8fe8c06af8c4d4479a7eabc1250
Andi <andicbuchanan@gmail.com>
2017-03-08 04:34:07 +0000

    basic set up

---
+++ index.php
@@ -0,0 +1,12 @@
+<html>
+<head>
+    <title>Plotpoints</title>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" href="/styles/main.css">
+    <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
+    <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js" integrity="sha256-0YPKAwZP7Mp3ALMRVB2i8GXeEndvCq3eSl/WsAl1Ryk=" crossorigin="anonymous"></script>
+</head>
+<body>
+    <p>Hello World</p>
+</body>
+</html>
