d9a0f00f4e14545ba0c1dd3d00d2632ed15ef6ca
Andi <andicbuchanan@gmail.com>
2017-03-08 05:26:25 +0000

    use of chips and font awesome

--- index.php
+++ index.php
@@ -4,10 +4,16 @@
     <meta charset="UTF-8">
     <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons">
     <link rel="stylesheet" href="https://code.getmdl.io/1.3.0/material.blue_grey-light_blue.min.css" />
-    <link rel="stylesheet" href="/styles/main.css">
-    <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+        <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
     <script src="https://code.jquery.com/jquery-3.1.1.js" integrity="sha256-16cdPddA6VdVInumRGo6IbivbERE8p7CQR3HzTBuELA=" crossorigin="anonymous"></script>
     <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js" integrity="sha256-0YPKAwZP7Mp3ALMRVB2i8GXeEndvCq3eSl/WsAl1Ryk=" crossorigin="anonymous"></script>
+    <script src="https://use.fontawesome.com/5462403d39.js"></script>
+    <script>
+        $( function() {
+            $( ".draggable" ).draggable();
+        } );
+    </script>
+    <link rel="stylesheet" href="/styles/main.css">
 </head>
 <body>
     <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
@@ -27,7 +33,23 @@
         </div>
       </header>
       <main class="mdl-layout__content">
-        <div class="page-content"><!-- Your content goes here --></div>
+        <div class="page-content">
+            <span class="mdl-chip mdl-chip--contact mdl-chip--deletable draggable ui-widget-content">
+                <span class="mdl-chip__contact mdl-color--brown mdl-color-text--white"><i class="fa fa-play" aria-hidden="true"></i></span>
+                <span class="mdl-chip__text">Quest starts</span>
+                <a href="#" class="mdl-chip__action"><i class="material-icons">cancel</i></a>
+            </span>
+            <span class="mdl-chip mdl-chip--contact mdl-chip--deletable draggable ui-widget-content">
+                <span class="mdl-chip__contact mdl-color--pink mdl-color-text--white"><i class="fa fa-heart" aria-hidden="true"></i></span>
+                <span class="mdl-chip__text">Meets love interest</span>
+                <a href="#" class="mdl-chip__action"><i class="material-icons">cancel</i></a>
+            </span>
+            <span class="mdl-chip mdl-chip--contact mdl-chip--deletable draggable ui-widget-content">
+                <span class="mdl-chip__contact mdl-color--red mdl-color-text--white"><i class="fa fa-bomb" aria-hidden="true"></i></span>
+                <span class="mdl-chip__text">Major battle</span>
+                <a href="#" class="mdl-chip__action"><i class="material-icons">cancel</i></a>
+            </span>
+        </div>
       </main>
     </div>
 </body>
