c61dadb7ae0938caef0f9a9e59a8f4d36d1ae18f
Andi <andicbuchanan@gmail.com>
2017-03-15 23:14:04 +0000

    connection to database working

--- connect.php
+++ connect.php
@@ -6,15 +6,32 @@
     $servername = getenv('IP');
     $username = getenv('C9_USER');
     $password = "";
-    $database = "c9";
+    $database = "plotpoints";
     $dbport = 3306;
 
     // Create connection
     $db = new mysqli($servername, $username, $password, $database, $dbport);
+    
+    
 
     // Check connection
     if ($db->connect_error) {
         die("Connection failed: " . $db->connect_error);
+        echo "Error!";
     } 
     echo "Connected successfully (".$db->host_info.")";
+    
+    $sql = "INSERT INTO points (id,text,thread,event,x,y) VALUES(2,\"engagement\",\"diamond\",\"Thread Three\",0,50)";
+    
+    if ($db->query($sql) === TRUE) {
+        echo "New record created successfully";
+    } else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+    }
+
+$conn->close();
 ?>
+
+<?php
+
+
